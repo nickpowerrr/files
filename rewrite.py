57c0a561e7b1fcb822a2b6fc979ce31f3b4354d8
nick <nickpowerrr@gmail.com>
2026-06-15 11:22:09 +0200

    h

--- rewrite.py
+++ rewrite.py
@@ -1,13 +1,12 @@
 print("starting injection..")
 import os
 def inject():
-    return r"print(CODE HAS BEEN INJECTED!!)"
+    return r"print('CODE HAS BEEN INJECTED!!')"
 
 path = os.path.dirname(os.path.abspath(__file__))
 new_file_path = os.path.join(path, "code.txt")
 with open(new_file_path, "w") as f:
     code = inject()
-    print(code)
     f.write(code)
            
         
